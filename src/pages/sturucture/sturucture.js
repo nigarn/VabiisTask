273d50033307a60bbc844b2ad9c6c0be2f318291
Nigar Niftaliyeva <nigar.n@azerimed.local>
2021-11-01 14:34:23 +0400

    filtering, sorting, searching, paging added

--- src/pages/sturucture/sturucture.js
+++ src/pages/sturucture/sturucture.js
@@ -1,7 +1,34 @@
 
+import React from 'react'
+import {
+  TreeList,
+  Editing,
+  Column,
+  Button,
+  HeaderFilter,
+  FilterPanel,
+  FilterRow,
+  Paging,Scrolling
+} from "devextreme-react/tree-list";
 
-
-export default function Sturucture() {
-  return (<div></div>)
-  
+const sturucture = () => {
+  return (
+    <TreeList
+    showBorders={true}
+  >
+    <HeaderFilter visible={true} />
+    <HeaderFilter visible={true} />
+    <FilterPanel visible={true} />
+    <FilterRow visible={true} />
+    <Scrolling mode="standard" />
+    <Column dataField="Test1"  caption="Tes1"/>
+    <Column dataField="Test2"  caption="Position"/>
+    <Column dataField="" />
+    
+    <Paging enabled={true} defaultPageSize={10} />
+   
+  </TreeList>
+  )
 }
+
+export default sturucture
